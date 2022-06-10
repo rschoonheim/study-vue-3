aaf5777a6601276a356e71fddaad1e1dafa9fe9d
Romano Schoonheim <stal@romanoschoonheim.nl>
2022-06-10 12:16:57 +0200

    Fix linting issues.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
+import "./App.css";
+
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
-
-import './index.css'
 
 const app = createApp(App);
 
